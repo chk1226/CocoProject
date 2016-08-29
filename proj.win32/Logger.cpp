195bc1950c0eff93ce08fc23841aeeb513dc1dc2
chk <chk1226@hotmail.com>
2016-08-29 16:45:36 +0800

    init

---
+++ proj.win32/Logger.cpp
@@ -0,0 +1,9 @@
+#include "Logger.h"
+
+using namespace MyFramework;
+
+template <class T>
+void DebugLog(T print)
+{
+	std::cout << print << std::endl;
+}
---
+++ proj.win32/Logger.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+namespace MyFramework 
+{
+	template <class T>
+	void DebugLog(T print);
+
+}
